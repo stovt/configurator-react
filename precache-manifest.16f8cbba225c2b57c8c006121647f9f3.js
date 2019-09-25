7ab15b061e200b9fc7e13c7de136925f660bcde3
Sergey Tovt <st@norse.co>
2019-09-25 15:47:49 +0300

    Updates

---
+++ precache-manifest.16f8cbba225c2b57c8c006121647f9f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef07707a53a31261674b178db5e8f726",
+    "url": "/configurator-react/index.html"
+  },
+  {
+    "revision": "0318391158b6cae56f0f",
+    "url": "/configurator-react/static/js/2.5034af91.chunk.js"
+  },
+  {
+    "revision": "53f022977b1407666192",
+    "url": "/configurator-react/static/js/main.f548f1cc.chunk.js"
+  },
+  {
+    "revision": "b911c7cb8483128e5db9",
+    "url": "/configurator-react/static/js/runtime-main.52db7cd8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/configurator-react/precache-manifest.dd8a71caa150addf5527a8a9ce9b24d8.js"
+  "/configurator-react/precache-manifest.16f8cbba225c2b57c8c006121647f9f3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/configurator-react/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
